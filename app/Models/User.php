f3252babe227b67efc8a8ed351ec39023ce4303d
tuan-bach123 <pham.tuan.bach@sun-asterisk.com>
2024-08-06 21:06:34 +0700

    done chapter4, ready to merge

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,10 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 // use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Illuminate\Foundation\Auth\User as Authenticatable;
+
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
@@ -49,6 +52,24 @@ class User extends Model
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => (
+                $attributes['first_name'] . $attributes['full_name']
+            ),
+        );
+    }
+
+    protected function username(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => (
+                Str::slug($value)
+            )
+        );
     }
 
     public function tasks(): HasMany
