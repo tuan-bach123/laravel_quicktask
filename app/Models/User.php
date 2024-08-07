09a53013f00e1307ddf0d6d50c3f313248a30b5e
tuan-bach123 <pham.tuan.bach@sun-asterisk.com>
2024-08-07 17:00:33 +0700

    chapter8 done, ready to merge

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,14 +5,14 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Foundation\Auth\User as Authenticatable;
+ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class User extends Model
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,20 @@
 <?php
 
-use App\Http\Controllers\TaskController;
-use App\Http\Middleware\CheckAdmin;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(CheckAdmin::class)->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/users', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('users', UserController::class)->withoutMiddleware(CheckAdmin::class);
-Route::resource('tasks', TaskController::class);
+require __DIR__.'/auth.php';
