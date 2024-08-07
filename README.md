9d2d17e494156bcd05feda78798e5787bc611faa
tuan-bach123 <pham.tuan.bach@sun-asterisk.com>
2024-08-07 13:03:02 +0700

    chapter7 done, ready to merge

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Answer for the question is in answer_task folder.
 
-Created new route for users and tasks with new controller for them.
+Created new middleware.
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TaskController;
+use App\Http\Middleware\CheckAdmin;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -8,15 +9,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-Route::get('/users/create', [UserController::class,'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::middleware(CheckAdmin::class)->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+});
 
-Route::resources([
-    'users' => UserController::class,
-    'tasks' => TaskController::class,
-]);
+Route::resource('users', UserController::class)->withoutMiddleware(CheckAdmin::class);
+Route::resource('tasks', TaskController::class);
